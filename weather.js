4b238c19ea53d4a9cca67297dbdb571f49b29da5
Manender Bhati <manenderbhati188@gmail.com>
2024-10-02 01:04:25 +0530

    changes in this

--- weather.js
+++ weather.js
@@ -7,39 +7,47 @@ const humidity=document.getElementById("humidity");
 const wind_kph=document.getElementById("wind_kph");
 const pathimage=document.getElementById("pathimage");
 const currentlocation=document.getElementById("currentlocation");
-const images_wheather=document.getElementById("wheather_img");
+const image=document.getElementById("image_create");
+const img2=document.getElementById("create_img2");
+const img3=document.getElementById("create_img3");
+const temp1=document.getElementById("temp1");
+const temp2=document.getElementById("temp2");
+let day1=document.getElementById("day1");
+let day2=document.getElementById("day2");
+const wheather_img1=document.getElementById("wheather_img1");
+const humidity1=document.getElementById("humidity1");
+const wind1=document.getElementById("wind1");
+const humidity2=document.getElementById("humidity2");
+const wind2=document.getElementById("wind2");
 
 let wheather="";
-const wheatherimg={
-    "sunny":"http://wallpapercave.com/wp/2khKQyP.jpg",
-    "rain":"https://wallpaperaccess.com/full/164284.jpg",
-    "summer":"https://getwallpapers.com/wallpaper/full/f/9/3/705058-wallpaper-summer-1920x1080-windows-10.jpg",
-    "windy":"https://getwallpapers.com/wallpaper/full/f/9/3/705058-wallpaper-summer-1920x1080-windows-10.jpg",
-    "stormy":"https://tse2.mm.bing.net/th?id=OIP.PylD5wxZ84TgeA79gPYdpwHaE4&pid=Api&P=0&h=220",
-    "mist":"http://www.zastavki.com/pictures/originals/2014/Nature___Seasons___Spring_Sunny_weather_in_the_spring_067863_.jpg"
-}
-
-
 let city="";
+let arr=[];
 
 const getlocation=async(location)=>{
     const response=await fetch(`https://api.weatherapi.com/v1/current.json?key=468e8ea0c27d4095aba104128243101&q=${location.coords.latitude},${location.coords.longitude}&aqi=yes`);
     let data=await response.json();
-    console.log(data);
     city=data.location.name;
     locationdata(city);
 }
 const notgetlocation=async ()=>{
     console.log("Location Not Found");
 }
+
 currentlocation.addEventListener('click',()=>{
     const result=navigator.geolocation.getCurrentPosition(getlocation,notgetlocation);
-    
 })
 
 button.addEventListener("click",()=>{
+
+    const city=inputvalue.value.trim();
+    if(city===""){
+        alert("Please Enter City");
+        return;
+    }
     locationdata(city);
     inputvalue.value="";
+
 })
 
 inputvalue.addEventListener('change',()=>{
@@ -49,28 +57,47 @@ inputvalue.addEventListener('change',()=>{
 
 const locationdata=async(city)=>{
     try{
-        const response=await fetch(`https://api.weatherapi.com/v1/current.json?key=468e8ea0c27d4095aba104128243101&q=${city}&aqi=yes`);
+        const response=await fetch(`http://api.weatherapi.com/v1/forecast.json?key=468e8ea0c27d4095aba104128243101&q=${city}&days=3&aqi=no&alerts=no`);
         const data=await response.json();
     
         if(data.location){
             cityname.innerText=data.location.name+" "+data.location.region+" "+data.location.country;
-            day.innerText=data.location.localtime.split(' ').reverse().join();
             temp.innerText=data.current.temp_c;
             humidity.innerText=data.current.humidity;
             wind_kph.innerText=data.current.wind_kph;
             wheather=data.current.condition.text;
-            {
-                Object.entries(wheatherimg).map(([key,value])=>{
-                    if(key==wheather.toLowerCase()){
-                        images_wheather.setAttribute('src',value);
-                        console.log(images_wheather);
-                    }
-                })
-            }
+            let days=data.forecast.forecastday;
+            Object.entries(days).map(([key,value])=>{
+                if(key==0){
+                    day.innerText=value.date;
+                    const img=document.createElement("img");
+                    img.setAttribute('src','http://wallpapercave.com/wp/2khKQyP.jpg');
+                    image.appendChild(img);
+                    value.day.avghumidity;
+                }
+                if(key==1){
+                    day1.innerText=value.date;
+                    temp1.innerText=value.day.avgtemp_c;
+                    const img=document.createElement("img");
+                    img.setAttribute('src','https://getwallpapers.com/wallpaper/full/f/9/3/705058-wallpaper-summer-1920x1080-windows-10.jpg');
+                    img2.appendChild(img);
+                    humidity1.innerText=value.day.avghumidity;
+                    wind1.innerText=value.day.maxwind_kph;
+                }
+                if(key==2){
+                    day2.innerText=value.date;
+                    temp2.innerText=value.day.avgtemp_c;
+                    const img=document.createElement("img");
+                    img.setAttribute('src','https://tse2.mm.bing.net/th?id=OIP.PylD5wxZ84TgeA79gPYdpwHaE4&pid=Api&P=0&h=220');
+                    img3.appendChild(img);
+                    humidity2.innerText=value.day.avghumidity;
+                    wind2.innerText=value.day.maxwind_kph;
+                }
+            })
             
         }
         else{
-            cityname.innerText="City Not Found";
+            cityname.innerText="Invalid City";
         }
     }
     catch(err){
